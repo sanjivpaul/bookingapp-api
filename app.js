542bb7be9eba720b251e5b5500292e128e96a47c
Sanjiv Paul <paulsanjiv81@gmail.com>
2023-06-10 16:53:27 +0530

    initialize booking app

---
+++ app.js
@@ -0,0 +1,29 @@
+// const express = require("express");
+// we are not using this style so we use type: module in json file
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+const app = express();
+dotenv.config();
+
+const PORT = 8800;
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        throw error;
+    }
+
+}
+
+app.listen(PORT, (req, res) => {
+    try {
+        connect();//call mogodb function
+        console.log("connected to server on port: ", PORT)
+    } catch (error) {
+        console.log(error);
+    }
+})
